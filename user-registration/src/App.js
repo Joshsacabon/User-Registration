f8fd0c0228cacb09498ef054ba4702e5f23c785e
Joshsacabon <sacabonj@gmail.com>
2021-08-21 11:21:04 +0800

    initial commit

---
+++ user-registration/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom'
+import { UserList } from './UserList'
+import { CreateUser } from './CreateUser'
+import { EditUser } from './EditUser'
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar bg-light navbar-expand-lg navbar-light">
+        <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">User List</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/create" className="nav-link">Create Todo</Link>
+          </li>
+        </ul>
+      </nav>
+    <Switch>
+      <Route exact path="/" component={UserList}/>
+      <Route path="/edit/:id" component={EditUser}/>
+      <Route path="/create" component={CreateUser}/>
+    </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-registration/src/UserList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const UserList = () => {
+    return(
+        <div className="container">UserList</div>
+    )
+}
